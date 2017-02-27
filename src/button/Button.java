1e1ac8c53292ed65854b2da633ff1015535ac5aa
Renan <Renan@192.168.0.2>
2017-02-27 18:56:44 -0300

    pequenas alterações

--- src/button/Button.java
+++ src/button/Button.java
@@ -1,5 +1,30 @@
 package button;
+import java.awt.Color;// usadas para encapsular cores no padrao RGB
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
 
-public class Button {
-
+public class Button extends JPanel{
+	
+	int x, y, width, height;
+	Image image;
+	Color color;
+	
+	public Button(int x, int y, int width, int height, Image image, Color color, MouseAdapter mouseAdapter) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.image = image;
+		this.color = color;
+		
+		setBounds(x, y, width, height);
+		
+		addMouseListener(mouseAdapter);
+	}
+	
 }
--- src/button/CircleButton.java
+++ src/button/CircleButton.java
@@ -1,5 +1,16 @@
 package button;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
 
-public class CircleButton {
-
+public class CircleButton extends JPanel{
+	
+	int x, y, diameter;
+	Color color;
+	Image image;
+	
 }
